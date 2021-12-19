9e1b715efdf85f58c059a40e130b3606600a1f85
Amansaini7 <56645620+Amansaini7@users.noreply.github.com>
2021-12-19 17:52:44 +0530

    RPS game added

--- app.js
+++ app.js
@@ -24,22 +24,38 @@ app.use(bodyParser.urlencoded ({ extended: true }))
 
 app.get("/", (req, res) => {
     res.render("home");
-})
+});
 
 
 app.get("/gamepage", (req, res) => {
     res.render("gamepage");
-})
+});
+
+
+
+// RPS game part 
+
+app.get('/rps_intro', function (req, res) {
+    res.sendFile('rps_intro.html', { root: './public/rps' });
+});
+
+app.get('/rps', function (req, res) {
+    res.sendFile('rps.html', { root: './public/rps' });
+});
+
+
+
 
 
 
 //Listen On Server
-app.listen(process.env.PORT || 3000, function (err) {
+port = 8080
+app.listen(process.env.PORT || port, function (err) {
     if (err) {
         console.log(err);
     } else {
         console.log("\n*** Connection Successfully Established ***")
-        console.log("Server Started At Port 3000");
+        console.log("Server Started At Port " + port);
     }
 
 });
